9c33b1ed0eee756b42891e556985c68588245865
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2020-11-22 15:14:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
     }
 }
-
 
 function topFunction() {
     document.body.scrollTop = 0;
